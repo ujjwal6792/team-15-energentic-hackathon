c7cf527292e3eff1ab2c431a30b8dabe95ef8ec9
Ujjwal Sharma <ujjwal.sharma@dhiway.com>
2025-05-16 13:10:41 +0530

    feat: beautify prompt ui

--- ui/src/components/ui/prompt-suggestions.tsx
+++ ui/src/components/ui/prompt-suggestions.tsx
@@ -11,13 +11,13 @@ export function PromptSuggestions({
 }: PromptSuggestionsProps) {
   return (
     <div className="space-y-3">
-      <h2 className="text-center text-lg font-semibold">{label}</h2>
-      <div className="flex gap-6 text-sm">
+      <h2 className="text-left text text-[#4c4f69]">{label}</h2>
+      <div className="flex px-4 flex-wrap gap-6 text-sm">
         {suggestions.map((suggestion) => (
           <button
             key={suggestion}
             onClick={() => append({ role: "user", content: suggestion })}
-            className="h-min flex-1 rounded-xl border border-muted bg-none cursor-pointer p-4 hover:bg-muted"
+            className="h-min flex-1 rounded-xl min-w-64 shadow bg-[#B6DDF6] text-indigo-900 cursor-pointer px-4 py-3 hover:bg-[#B6DDF675]"
           >
             <p>{suggestion}</p>
           </button>
